9019d52a8c586374057901eb1efc0ab9eda405ac
Bbbtt04 <2393714045@qq.com>
2023-03-03 20:20:23 +0800

    ✨特性: 页面请求并渲染

---
+++ src/pages/page/[id].vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import request from '@/utils/request'
+
+const route = useRoute()
+const { id } = route.params
+const page: any = ref({})
+
+onMounted(async () => {
+  const { data } = await request({
+    url: `/page/${id}`,
+    method: 'get',
+  })
+  page.value = data
+})
+</script>
+
+<template>
+  <div>
+    <Markdown :content="page.content" />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
